1477108f400f5adbcfebf9c7db571edfb6fc8c40
liumaohua@15sms.com <574425839@qq.com>
2016-12-28 09:32:42 +0800

    使用`xcodebuild -exportArchive`替换PackageApplication进行打包
    使用`xcodebuild
    -exportArchive`替换PackageApplication进行打包，去除-target和-output传参，默认在桌面创建如{sch
    eme}{2016-12-28_08-08-10}的ipa文件输出目录。

---
+++ autobuild/README.md
@@ -0,0 +1,50 @@
+#autobuild.py
+
+iOS 自动化打包脚本，并上传*ipa*文件至蒲公英。参数说明:
+
+```
+Usage: autobuild.py [options]
+
+Options:
+  -h, --help            show this help message and exit
+  -w name.xcworkspace, --workspace=name.xcworkspace
+                        Build the workspace name.xcworkspace.
+  -p name.xcodeproj, --project=name.xcodeproj
+                        Build the project name.xcodeproj.
+  -s schemename, --scheme=schemename
+                        Build the scheme specified by schemename. Required if
+                        building a workspace.
+```
+
+使用方式:
+```
+./autobuild.py -p youproject.xcodeproj -s schemename
+或者
+./autobuild.py -w youproject.xcworkspace -s schemename
+```
+
+脚本中有几个全局变量，根据自己项目设置进行修改其值, 包括:
+
+```
+CONFIGURATION = "Release"
+EXPORT_OPTIONS_PLIST = "exportOptions.plist"
+#会在桌面创建输出ipa文件的目录
+EXPORT_MAIN_DIRECTORY = "~/Desktop/"
+```
+
+*CONFIGURATION*：可在项目工程文件所在目录执行`xcodebuild -list`查看所有可选取值。
+*EXPORT_OPTIONS_PLIST*：导出*ipa*文件时的配置参数，该参数值是你的配置参数文件名，请在*autobuild.py*文件所在目录下创建*exportOptions.plist*文件，配置参数可使用`xcodebuild --help`查看所有可选取值。
+*EXPORT_MAIN_DIRECTORY*：默认情况下会在桌面创建*ipa*文件导出的文件夹，文件夹命名如:*~/Desktop/{scheme}{2016-12-28_08-08-10}*。
+
+```
+# configuration for pgyer
+PGYER_UPLOAD_URL = "http://www.pgyer.com/apiv1/app/upload"
+DOWNLOAD_BASE_URL = "http://www.pgyer.com"
+USER_KEY = "15d6xxxxxxxxxxxxxxxxxx"
+API_KEY = "efxxxxxxxxxxxxxxxxxxxx"
+#设置从蒲公英下载应用时的密码
+PYGER_PASSWORD = "" 
+```
+上传*ipa*文件至蒲公英的配置，你需要设置的是:*USER_KEY*, *API_KEY*, *PYGER_PASSWORD*。
+
+相关说明见我的博客[iOS自动打包并发布脚本](http://liumh.com/2015/11/25/ios-auto-archive-ipa/)
---
+++ autobuild/autobuild.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#./autobuild.py -p youproject.xcodeproj -s schemename
+#./autobuild.py -w youproject.xcworkspace -s schemename
+
+import argparse
+import subprocess
+import requests
+
+#configuration for iOS build setting
+CONFIGURATION = "Debug"
+EXPORT_OPTIONS_PLIST = "exportOptions.plist"
+#会在桌面创建输出ipa文件的目录
+EXPORT_MAIN_DIRECTORY = "~/Desktop/"
+
+# configuration for pgyer
+PGYER_UPLOAD_URL = "http://www.pgyer.com/apiv1/app/upload"
+DOWNLOAD_BASE_URL = "http://www.pgyer.com"
+USER_KEY = "15d6xxxxxxxxxxxxxxxxxx"
+API_KEY = "efxxxxxxxxxxxxxxxxxxxx"
+#设置从蒲公英下载应用时的密码
+PYGER_PASSWORD = "" 
+
+def cleanArchiveFile(archiveFile):
+	cleanCmd = "rm -r %s" %(archiveFile)
+	process = subprocess.Popen(cleanCmd, shell = True)
+	process.wait()
+	print "cleaned archiveFile: %s" %(archiveFile)
+
+
+def parserUploadResult(jsonResult):
+	resultCode = jsonResult['code']
+	if resultCode == 0:
+		downUrl = DOWNLOAD_BASE_URL +"/"+jsonResult['data']['appShortcutUrl']
+		print "Upload Success"
+		print "DownUrl is:" + downUrl
+	else:
+		print "Upload Fail!"
+		print "Reason:"+jsonResult['message']
+
+def uploadIpaToPgyer(ipaPath):
+    print "ipaPath:"+ipaPath
+    files = {'file': open(ipaPath, 'rb')}
+    headers = {'enctype':'multipart/form-data'}
+    payload = {'uKey':USER_KEY,'_api_key':API_KEY,'publishRange':'2','isPublishToPublic':'2', 'password':PYGER_PASSWORD}
+    print "uploading...."
+    r = requests.post(PGYER_UPLOAD_URL, data = payload ,files=files,headers=headers)
+    if r.status_code == requests.codes.ok:
+         result = r.json()
+         parserUploadResult(result)
+    else:
+        print 'HTTPError,Code:'+r.status_code
+
+#创建输出ipa文件路径: ~/Desktop/{scheme}{2016-12-28_08-08-10}
+def buildExportDirectory(scheme):
+	dateCmd = 'date "+%Y-%m-%d_%H-%M-%S"'
+	process = subprocess.Popen(dateCmd, stdout=subprocess.PIPE, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+	exportDirectory = "%s%s%s" %(EXPORT_MAIN_DIRECTORY, scheme, stdoutdata.strip())
+	return exportDirectory
+
+def buildArchivePath(tempName):
+	process = subprocess.Popen("pwd", stdout=subprocess.PIPE)
+	(stdoutdata, stderrdata) = process.communicate()
+	archiveName = "%s.xcarchive" %(tempName)
+	archivePath = stdoutdata.strip() + '/' + archiveName
+	return archivePath
+
+def getIpaPath(exportPath):
+	cmd = "ls %s" %(exportPath)
+	process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+	ipaName = stdoutdata.strip()
+	ipaPath = exportPath + "/" + ipaName
+	return ipaPath
+
+def buildProject(project, scheme):
+	archivePath = buildArchivePath(scheme)
+	print "archivePath: " + archivePath
+	archiveCmd = 'xcodebuild -project %s -scheme %s -configuration %s archive -archivePath %s -destination generic/platform=iOS' %(project, scheme, CONFIGURATION, archivePath)
+	process = subprocess.Popen(archiveCmd, shell=True)
+	process.wait()
+
+	exportDirectory = buildExportDirectory(scheme)
+	signCmd = "xcodebuild -exportArchive -archivePath %s -exportPath %s -exportOptionsPlist %s" %(archivePath, exportDirectory, EXPORT_OPTIONS_PLIST)
+	process = subprocess.Popen(signCmd, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+
+	ipaPath = getIpaPath(exportDirectory)
+	uploadIpaToPgyer(ipaPath)
+	cleanArchiveFile(archivePath)
+
+def buildWorkspace(workspace, scheme):
+	archivePath = buildArchivePath(scheme)
+	print "archivePath: " + archivePath
+	archiveCmd = 'xcodebuild -workspace %s -scheme %s -configuration %s archive -archivePath %s -destination generic/platform=iOS' %(workspace, scheme, CONFIGURATION, archivePath)
+	process = subprocess.Popen(archiveCmd, shell=True)
+	process.wait()
+
+	exportDirectory = buildExportDirectory(scheme)
+	signCmd = "xcodebuild -exportArchive -archivePath %s -exportPath %s -exportOptionsPlist %s" %(archivePath, exportDirectory, EXPORT_OPTIONS_PLIST)
+	process = subprocess.Popen(signCmd, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+
+	ipaPath = getIpaPath(exportDirectory)
+	uploadIpaToPgyer(ipaPath)
+	cleanArchiveFile(archivePath)
+
+def xcbuild(options):
+	project = options.project
+	workspace = options.workspace
+	scheme = options.scheme
+
+	if project is None and workspace is None:
+		pass
+	elif project is not None:
+		buildProject(project, scheme)
+	elif workspace is not None:
+		buildWorkspace(workspace, scheme)
+
+def main():
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-w", "--workspace", help="Build the workspace name.xcworkspace.", metavar="name.xcworkspace")
+	parser.add_argument("-p", "--project", help="Build the project name.xcodeproj.", metavar="name.xcodeproj")
+	parser.add_argument("-s", "--scheme", help="Build the scheme specified by schemename. Required if building a workspace.", metavar="schemename")
+
+	options = parser.parse_args()
+
+	print "options: %s" % (options)
+
+	xcbuild(options)
+
+if __name__ == '__main__':
+	main()
